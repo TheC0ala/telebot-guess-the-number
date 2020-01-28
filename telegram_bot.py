cff24546782fcae34c37b4788502f599fdda38f1
miguel-olivero <github@molivero.com>
2020-01-28 10:21:41 +0100

    Using a hidden file for the key

--- telegram_bot.py
+++ telegram_bot.py
@@ -116,7 +116,8 @@ def main():
 	# Create the Updater and pass it your bot's token.
 	# Make sure to set use_context=True to use the new context based callbacks
 	# Post version 12 this will no longer be necessary
-	updater = Updater(TELE_KEY, use_context=True)
+	import telekey
+	updater = Updater(telekey.telekey, use_context=True)
 
 	# Get the dispatcher to register handlers
 	dp = updater.dispatcher
